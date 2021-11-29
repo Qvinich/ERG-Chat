82287128d38035b8ec92aecd3f9631752083160a
Qvinich <93231950+Qvinich@users.noreply.github.com>
2021-11-29 08:13:54 +0700

    Add files via upload

---
+++ chatsapps/php/search.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(0);
+
+    session_start();
+    include_once "config.php";
+
+    $outgoing_id = $_SESSION['unique_id'];
+    $searchTerm = mysqli_real_escape_string($conn, $_POST['searchTerm']);
+    $output = "";
+    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} AND (fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%') ";
+    if(mysqli_num_rows($query) > 0){
+        include "data.php";
+    }else{
+        $output .= 'No user found related to your search term';
+    }
+    echo $output;
+?>
